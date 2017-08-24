8e203c8b657d20d50106ad9fc3d2d3da60d32e41
Danilo Cunha <danilokage@gmail.com>
2017-08-24 17:13:07 -0300

    Get details frame

--- search_card.py
+++ search_card.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
 def card_url(card_name):
     url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&btSubmit=btSubmit&card=" + card_name
     return url
@@ -16,9 +17,18 @@ def get_card(card_name):
 card_data = {}
 
 # Capture Search Word and append to dictionary
-search_words = 'stifle'
+search_words = "mirari's wake"
 card_data["search_words"] = search_words
 card_data["url_liga"] = "LigaMagic:" + card_url(search_words)
+
+
+# ------CARD FOUND ------
+# ------CARD FOUND ------
+# ------CARD FOUND ------
+
+# ------CARD-------
+# ------CARD-------
+# ------CARD-------
 
 # Gets text through LigaMagic web request
 soup = BeautifulSoup(get_card(search_words), "lxml")
@@ -37,6 +47,20 @@ image = card_frame.find("span", {"id": "omoImage"})
 image = [x['src'] for x in image.find_all('img')]
 card_data["img"] = image
 
+# -----CARD DETAILS-----
+# -----CARD DETAILS-----
+# -----CARD DETAILS-----
 
-print(card_data)
+# Card details frame div
+card_details_frame = soup.find("div", {"class": "card-detalhes"}).find("div")
 
+card_details = card_details_frame.findAll("p")
+# imagecost = [x['src'] for x in card_details.findAll('img')]
+
+# Grabs price for each edition
+
+# print (card_details_frame.text)
+for lines in card_details:
+    print(lines)
+    print("\n\n\n")
+# print (card_data)
