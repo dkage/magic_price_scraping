7e3bdaf51a9eb7ee56358545eedc58976c693dcb
Danilo Cunha <danilokage@gmail.com>
2019-09-25 19:30:36 -0300

    Add start of new class and refactor of imports

--- bot_functions.py
+++ bot_functions.py
@@ -1,7 +1,12 @@
-from api_key import TOKEN
-import json
 import requests
-import urllib.parse
-import time
+from api_key import TELEGRAM_KEY
 
-#TODO use new bot as ground
+
+class Telegram:
+
+    def __init__(self):
+        self.url = "https://api.telegram.org/bot{}/".format(TELEGRAM_KEY)
+
+        self.offset = ''
+        self.name = ''
+
