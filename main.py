aad0452164a00e57e991a15db49617fc63552296
Danilo Cunha <danilokage@gmail.com>
2019-09-27 23:05:31 -0300

    Add get_updates function for new class

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tbot import Telegram
 from time import sleep
 
+
 def main():
-    return True
+    while True:
+        update = bot.get_updates()
+
+        print(update)
+        sleep(10)
 
 
 if __name__ == '__main__':
@@ -10,4 +15,6 @@ if __name__ == '__main__':
     while not bot.get_me():
         print('Bot is offline for some reason, error during get_me checkup.')
         sleep(20)
-    print('Bot ok')
+    print('Bot online.')
+    main()
+
--- tbot.py
+++ tbot.py
@@ -10,11 +10,16 @@ class Telegram:
 
         self.offset = ''
         self.name = ''
+        self.bot_id = ''
+        self.updates_json = '0'
 
     def get_me(self):
         get_me = self.url + 'getme'
 
         http_return = requests.get(get_me).json()
+
+        self.name = http_return['result']['username']
+        self.bot_id = http_return['result']['id']
 
         if http_return['ok']:
             return True
@@ -22,10 +27,23 @@ class Telegram:
             return False
 
     def send_message(self):
-        # TODO scope
         return True
-
 
     def get_updates(self):
-        #TODO scope
-        return True
+        update_url = self.url + 'getUpdates?timeout=100'
+
+        # If offset is already set, concatenates it to tell API last ID already received
+        if self.offset:
+            update_url += "&offset={}".format(self.offset)
+        http_response = requests.get(update_url)
+        json_response = http_response.json()
+        self.updates_json = json_response['result']
+
+        # Set variable offset to tell API url, the last update ID already received
+        print(json_response)
+        self.offset = int(json_response['result'][-1]['update_id']) + 1
+
+        if json_response['ok']:
+            return json_response['result']
+        else:
+            return False
